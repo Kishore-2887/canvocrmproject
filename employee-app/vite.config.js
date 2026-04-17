103cd88afde9a274277657731c8a28eb4405fda6
Kishore <kishore@cytrusst.com>
2026-04-18 00:35:28 +0530

    removed local proxy settings

--- employee-app/vite.config.js
+++ employee-app/vite.config.js
@@ -5,8 +5,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3002,
-    proxy: {
-      '/api': { target: 'http://localhost:5000', changeOrigin: true },
-    },
   },
 });
